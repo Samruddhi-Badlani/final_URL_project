f46cbecfc493c2c2746deac0a3b19daceb901c71
Samruddhi-Badlani <samruddhi4227132@gmail.com>
2021-07-22 13:12:56 +0530

    added the code to check the Copyright text is there in html response or not

--- app.js
+++ app.js
@@ -10,11 +10,11 @@ require('dotenv').config();
 var pool;
 var client;
 app = express();
-let my_user= process.env.USER;
+let my_user = process.env.USER;
 let my_host = process.env.HOST;
 let my_port_db = process.env.PORT;
-let my_password= process.env.PASSWORD;
-let my_databse= process.env.DATABASE;
+let my_password = process.env.PASSWORD;
+let my_databse = process.env.DATABASE;
 //DB connection
 var connect = "postgres://user1:abc1234@localhost/my_database";
 
@@ -29,37 +29,47 @@ app.use(express.urlencoded({ extended: false }));
 //Server 
 my_initial_list = null;
 app.get('/', function (req, res) {
-  
+
   client.query('SELECT * FROM url_status', (err, result) => {
     res.render('index', { urls: result.rows });
-   
+
   })
 
 });
 
 app.post('/example', (req, res) => {
-  
+
   client.query("INSERT INTO url_status(url_name,status) VALUES('" + req.body.add_url + "','closing')", (err, result) => {
     if (err) {
       console.log("Error");
       console.log(err)
     }
   });
-  var check=0;
-  request.get(req.body.add_url,function(error,response,body){
+  var check = 0;
+  request.get(req.body.add_url, function (error, response, body) {
     if (error || response === null) {
       console.log("Closing");
-      client.query("UPDATE url_status SET status='closing' WHERE url_name=$1",[req.body.add_url], (err, temp) => {
+      client.query("UPDATE url_status SET status='closing' WHERE url_name=$1", [req.body.add_url], (err, temp) => {
         if (err)
           console.log(err);
       });
     }
     else if (response.statusCode == 200) {
       console.log("running");
-      client.query("UPDATE url_status SET status='running' WHERE url_name=$1",[req.body.add_url], (err, temp) => {
-        if (err)
-          console.log(err);
-      });
+          let my_html_response = JSON.stringify(response);
+          let flag = my_html_response.includes("Copyright");
+          if (flag) {
+            client.query("UPDATE url_status SET status='running' WHERE url_name=$1", [req.body.add_url] , (err, temp) => {
+              if (err)
+                console.log(err);
+            });
+          }
+          else{
+            console.log("Copyright not found in the url "+ [req.body.add_url]);
+            client.query("UPDATE url_status SET status='closing' WHERE url_name=$1", [req.body.add_url], (err, temp) => {
+
+            });
+          }
     }
     else {
       console.log("Closing else ")
@@ -74,11 +84,11 @@ app.post('/example', (req, res) => {
       }
       res.render('index', { urls: result.rows });
       console.log(req.body);
-      
+
     });
   })
-  
- 
+
+
 
 })
 app.post('/del', (req, res) => {
@@ -96,13 +106,13 @@ app.post('/del', (req, res) => {
     }
     res.render('index', { urls: result.rows });
     console.log(req.body);
-  
+
   });
 })
 
 app.get('/status', (req, res) => {
 
-  
+
   //  client.query("SELECT url_name FROM url_status WHERE id = 2").then((result)=>{
   //    console.log(result.rows[0]['url_name']);
   //    client.query("SELECT id FROM url_status WHERE url_name= $1",[result.rows[0]['url_name']],(error,res)=>{
@@ -112,20 +122,20 @@ app.get('/status', (req, res) => {
   //    })
   //  })
   client.query("SELECT * FROM url_status").then((res1) => {
-    
+
 
     for (let x = 0; x < res1.rows.length; x++) {
       // console.log(res1.rows[x]['url_name']);
       // console.log(x + "bool");
-      var link=""+res1.rows[x]['url_name'];
+      var link = "" + res1.rows[x]['url_name'];
       // console.log("https://www.google.com"+"  "+link);
       // console.log("https://www.google.com" == link)
-      var status_string = ""+res1.rows[x]['status'];
-     
-      
-      
+      var status_string = "" + res1.rows[x]['status'];
+
+
+
       request.get(link, function (error, response, body) {
-        
+
         if (error || response === null) {
           console.log("Closing");
           client.query("UPDATE url_status SET status='closing' WHERE url_name=$1", [res1.rows[x]['url_name']], (err, temp) => {
@@ -135,10 +145,20 @@ app.get('/status', (req, res) => {
         }
         else if (response.statusCode == 200) {
           console.log("running");
-          client.query("UPDATE url_status SET status='running' WHERE url_name=$1", [res1.rows[x]['url_name']], (err, temp) => {
-            if (err)
-              console.log(err);
-          });
+          let my_html_response = JSON.stringify(response);
+          let flag = my_html_response.includes("Copyright");
+          if (flag) {
+            client.query("UPDATE url_status SET status='running' WHERE url_name=$1", [res1.rows[x]['url_name']], (err, temp) => {
+              if (err)
+                console.log(err);
+            });
+          }
+          else{
+            console.log("Copyright not found in the url "+[res1.rows[x]['url_name']]);
+            client.query("UPDATE url_status SET status='closing' WHERE url_name=$1", [res1.rows[x]['url_name']], (err, temp) => {
+
+            });
+          }
         }
         else {
           console.log("Closing else ")
@@ -146,7 +166,7 @@ app.get('/status', (req, res) => {
 
           });
         }
-        if(x+1==res1.rows.length){
+        if (x + 1 == res1.rows.length) {
           client.query("SELECT * FROM url_status", (err, result) => {
             if (err) {
               console.log("Error");
@@ -154,49 +174,49 @@ app.get('/status', (req, res) => {
             }
             res.render('index', { urls: result.rows });
             console.log(result.rows);
-          
+
           });
         }
       })
-    
-      
-      
+
+
+
 
 
 
     }
-    
+
 
 
   })
- 
+
 
 
 
 })
 
-app.post('/edit',(req,res)=>{
-  
+app.post('/edit', (req, res) => {
+
   console.log(req.body.edited_url_id);
-  const values = [req.body.edited_url,req.body.edited_url_id]
+  const values = [req.body.edited_url, req.body.edited_url_id]
   console.log(req.body.edited_url);
-  client.query("UPDATE url_status SET url_name=$1,status='closing' WHERE id=$2",values,(err,result1)=>{
+  client.query("UPDATE url_status SET url_name=$1,status='closing' WHERE id=$2", values, (err, result1) => {
     console.log("hello");
-    if(err){
+    if (err) {
       console.log(err);
     }
-    })
-  request.get(req.body.edited_url,function(error,response,body){
+  })
+  request.get(req.body.edited_url, function (error, response, body) {
     if (error || response === null) {
       console.log("Closing");
-      client.query("UPDATE url_status SET status='closing' WHERE url_name=$1",[req.body.edited_url], (err, temp) => {
+      client.query("UPDATE url_status SET status='closing' WHERE url_name=$1", [req.body.edited_url], (err, temp) => {
         if (err)
           console.log(err);
       });
     }
     else if (response.statusCode == 200) {
       console.log("running");
-      client.query("UPDATE url_status SET status='running' WHERE url_name=$1",[req.body.edited_url], (err, temp) => {
+      client.query("UPDATE url_status SET status='running' WHERE url_name=$1", [req.body.edited_url], (err, temp) => {
         if (err)
           console.log(err);
       });
@@ -214,26 +234,26 @@ app.post('/edit',(req,res)=>{
       }
       res.render('index', { urls: result.rows });
       console.log(req.body);
-      
+
     });
   })
 })
 
 app.listen(3000, () => {
   const { Pool, Client } = require('pg')
-   pool = new Pool({
+  pool = new Pool({
     user: process.env.USER,
     host: process.env.HOST,
-    database:process.env.DATABASE,
-    password:process.env.PASSWORD,
-    port:process.env.PORT,
+    database: process.env.DATABASE,
+    password: process.env.PASSWORD,
+    port: process.env.PORT,
   })
-   client = new Client({
-    user:  my_user,
-    host:my_host,
-    database:my_databse,
-    password:my_password,
-    port:my_port_db
+  client = new Client({
+    user: my_user,
+    host: my_host,
+    database: my_databse,
+    password: my_password,
+    port: my_port_db
   })
   client.connect();
   console.log("Server started");
